01d42fb4ad63fa8651a6547473ad3a34cba522c5
Alby-On <66146586+Alby-On@users.noreply.github.com>
2026-03-22 15:05:19 -0300

    Update components.js

--- src/js/components.js
+++ src/js/components.js
@@ -11,15 +11,15 @@ async function includeComponent(containerId, filePath) {
 }
 
 // Así los llamas:
-includeComponent('navbar-container', '/src/components/navbar.html');
-includeComponent('header-container', '/src/components/header.html');
-includeComponent('carrusel-marcas-container', '/src/components/carrusel-marcas.html');
+includeComponent('navbar-container', 'src/components/navbar.html');
+includeComponent('header-container', 'src/components/header.html');
+includeComponent('carrusel-marcas-container', 'src/components/carrusel-marcas.html');
 
-includeComponent('nosotros-container', '/src/components/nosotros.html');
-includeComponent('clientes-container', '/src/components/clientes.html');
-includeComponent('servicios-container', '/src/components/servicios.html');
-includeComponent('porque-elegirnos-container', '/src/components/porque-elegirnos.html');
-includeComponent('contacto-container', '/src/components/contacto.html');
+includeComponent('nosotros-container', 'src/components/nosotros.html');
+includeComponent('clientes-container', 'src/components/clientes.html');
+includeComponent('servicios-container', 'src/components/servicios.html');
+includeComponent('porque-elegirnos-container', 'src/components/porque-elegirnos.html');
+includeComponent('contacto-container', 'src/components/contacto.html');
 
-includeComponent('footer-container', '/src/components/footer.html');
-includeComponent('wsp-container', '/src/components/wsp.html');
+includeComponent('footer-container', 'src/components/footer.html');
+includeComponent('wsp-container', 'src/components/wsp.html');
